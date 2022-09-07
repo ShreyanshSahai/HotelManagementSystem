f482a199c557a190a8ca8784ba2410c92954f76e
Shreyansh Sahai <66306217+ShreyanshSahai@users.noreply.github.com>
2022-09-07 20:28:19 +0530

    added dashboards

---
+++ HotelManagementSystem/Controllers/BookingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementSystem.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult BookRoom()
+        {
+
+            return View();
+        }
+    }
+}
